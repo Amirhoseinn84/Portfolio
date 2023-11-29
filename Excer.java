9edfdee4b6115049c0cff4f9a9246e02208fae19
Amirhosein Halimi <130469470+Amirhoseinn84@users.noreply.github.com>
2023-11-29 09:15:07 +0330

    Update Excer.java

---
+++ Excer.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RectangleAreaCalculator 
+
+    public static void main(String[] args) 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the length of the rectangle: ");
+        double length;
+length = scanner.nextDouble();
+
+        System.out.print("Enter the width of the rectangle: ");
+        double width;
+width = scanner.nextDouble();
+
+        double area;
+area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
